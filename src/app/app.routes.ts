ffbcbecaccedbb104e693191fbcbdae5cdf0b3c1
kashif <kashifshah5898@gmail.com>
2026-01-23 14:38:09 +0500

    feat: Implement authentication guards and update routing for protected routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,25 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { MinistryDetailComponent } from './components/ministry-detail/ministry-detail.component';
 import { ManageDigitalAssetsComponent } from './components/manage-digital-assets/manage-digital-assets.component';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginGuard } from './guards/login.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'ministry-detail', component: MinistryDetailComponent },
-  { path: 'add-digital-assets', component: ManageDigitalAssetsComponent },
+  { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
+  { 
+    path: 'dashboard', 
+    component: DashboardComponent, 
+    canActivate: [AuthGuard] 
+  },
+  { 
+    path: 'ministry-detail', 
+    component: MinistryDetailComponent, 
+    canActivate: [AuthGuard] 
+  },
+  { 
+    path: 'add-digital-assets', 
+    component: ManageDigitalAssetsComponent, 
+    canActivate: [AuthGuard] 
+  },
 ];
