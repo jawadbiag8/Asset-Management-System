49aacee26e977d1ade470b429502a722ca3792ef
jawadbaig8 <jawadbaig8@gmail.com>
2026-01-27 16:08:36 +0500

    fix: increase maximum size limits for component styles in angular.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mercurialminds.com/api',
+  apiUrl: 'http://54.179.187.5:7008/api',
   appName: 'Asset Management System',
   version: '1.0.0'
 };
