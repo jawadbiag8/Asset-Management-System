e50bb91e067df3b2ec71050daf0768f4b8ea2a07
kashif <kashifshah5898@gmail.com>
2026-01-29 16:57:59 +0500

    fix: update API URL in environment configuration
    - Changed the production API URL from 'http://54.179.187.5:7008/api' to 'http://54.255.207.60:7008/api'.
    - Removed commented-out API URL in the development environment for clarity.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://54.179.187.5:7008/api',
+  apiUrl: 'http://54.255.207.60:7008/api',
   appName: 'Asset Management System',
   version: '1.0.0'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://54.179.187.5:7008/api',
-  // apiUrl: 'http://192.168.1.150:2015/api',
   appName: 'Asset Management System',
   version: '1.0.0',
 };
